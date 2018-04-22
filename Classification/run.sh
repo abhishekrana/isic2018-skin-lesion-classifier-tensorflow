e3fce551df74f9276f94aa7f99fe0f6513be0d91
Abhishek Rana <abhishekrana502@gmail.com>
2018-04-22 18:13:39 +0200

    Code for cifar10 dataset classification added

--- Classification/run.sh
+++ Classification/run.sh
@@ -1,5 +1,6 @@
 # Cleanup
-# rm -vrf output/knifey_spoony/*
+rm -vrf output/cifar10/*
 
 # Run training
-python mains/main_knifey_spoony.py -c configs/config_knifey_spoony.json
+# python mains/main_knifey_spoony.py -c configs/config_knifey_spoony.json
+python mains/main_cifar10.py -c configs/config_cifar10.json
