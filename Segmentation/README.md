39d1123c773bae8ce15baa26e34bffefbbca9dc8
Abhishek Rana <abhishekrana502@gmail.com>
2018-04-24 22:35:39 +0200

    Code restructuring

--- Segmentation/README.md
+++
@@ -1,2 +0,0 @@
-# ISIC 2018 Skin Image Analysis Challenge
-http://workshop2018.isic-archive.com
--- Segmentation/run.sh
+++
@@ -1,6 +0,0 @@
-# Cleanup
-# rm -vrf output/knifey_spoony/*
-
-# Run training
-# python mains/main_knifey_spoony.py -c configs/config_knifey_spoony.json
-python mains/main_unet.py -c configs/config_unet.json
